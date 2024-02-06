6f87a8ca2dd438b248e80c21ba41c83cc1467023
patrickSevans123 <patricksamuel2107@gmail.com>
2024-02-06 08:59:12 +0700

    first push

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.eshop.service;
-
-import id.ac.ui.cs.advprog.eshop.model.Product;
-import java.util.List;
-
-public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-}
