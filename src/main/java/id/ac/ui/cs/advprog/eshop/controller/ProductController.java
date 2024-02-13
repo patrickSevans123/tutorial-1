a981dee9593dfb745990403ff1156d1b44270b4b
patrickSevans123 <patricksamuel2107@gmail.com>
2024-02-13 23:17:00 +0700

    fix some warnings

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -4,8 +4,11 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public void delete(String id);
-    public Product findById(String id);
-    public void edit(Product editedProduct);
-    public List<Product> findAll();
+    Product create(Product product);
+    void delete(String id);
+    Product findById(String id);
+    void edit(Product editedProduct);
+    List<Product> findAll();
 }
