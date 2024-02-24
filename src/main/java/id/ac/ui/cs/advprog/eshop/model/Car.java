735773a6929b7428140711118ef01399211f7ceb
patrickSevans123 <patricksamuel2107@gmail.com>
2024-02-24 15:53:47 +0700

    add constructor for Car object

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.UUID;
 
 
 @Getter
@@ -10,4 +11,9 @@ public class Car {
     private String carName;
     private String carColor;
     private int carQuantity;
+
+    public Car() {
+        UUID uuid = UUID.randomUUID();
+        this.setCarId(uuid.toString());
+    }
 }
