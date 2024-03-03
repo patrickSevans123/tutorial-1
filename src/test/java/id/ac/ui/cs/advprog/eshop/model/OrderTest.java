98c806a44b36c47642ce4870a80544540348ff15
patrickSevans123 <patricksamuel2107@gmail.com>
2024-03-03 17:47:45 +0700

    [GREEN] Implement Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -49,7 +49,7 @@ class OrderTest {
         assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
         assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
 
-        assertEquals("13652556-0128-4c87-b546-54eb1396d79b", order.getId());
+        assertEquals("13652556-012a-4c07-b546-54eb1396d796", order.getId());
         assertEquals(1708560000L, order.getOrderTime());
         assertEquals("Safira Sudrajat", order.getAuthor());
         assertEquals("WAITING_PAYMENT", order.getStatus());
