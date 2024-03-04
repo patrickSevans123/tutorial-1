eb61fd012dd02c6e26e9385311b14df15868b2a6
patrickSevans123 <patricksamuel2107@gmail.com>
2024-03-04 23:10:48 +0700

    [RED] Add OrderServiceImpl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+
+import java.util.List;
+
+public class OrderServiceImpl {
+    public Order createOrder (Order order){return null;}
+    public Order updateStatus (String orderId, String status){return null;}
+    public Order findById(String orderId){return null;}
+    public List<Order> findAllByAuthor (String author){return null;}
+}
