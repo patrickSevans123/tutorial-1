e3950cf9f2feca370e5aade81b4da6711ebbda8e
patrickSevans123 <patricksamuel2107@gmail.com>
2024-03-05 17:41:11 +0700

    [GREEN] implement order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,22 +1,73 @@
 package id.ac.ui.cs.advprog.eshop.model;
+
 import lombok.Builder;
 import lombok.Getter;
-
 import java.util.Map;
 
 @Builder
 @Getter
 public class Payment {
     String id;
-    String method;
-    String status;
-    Map<String, String> paymentData;
     Order order;
-
+    String method;
+    Map<String, String> paymentData;
+    String status;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+
+        boolean valid = false;
+        if (method.equals("VOUCHER_CODE")) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode != null && voucherCode.length() == 16 && voucherCode.startsWith("ESHOP")) {
+                int numCounter = 0;
+                for (char currentChar : voucherCode.toCharArray()) {
+                    if (Character.isDigit(currentChar)) {
+                        numCounter++;
+                    }
+                }
+                if (numCounter == 8) {
+                    valid = true;
+                }
+            }
+        } else if (method.equals("CASH_ON_DELIVERY")) {
+            String address = paymentData.get("address");
+            String deliveryFee = paymentData.get("deliveryFee");
+
+            if (address != null && !address.isBlank() && deliveryFee != null && !deliveryFee.isBlank()) {
+                valid = true;
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        if (valid){
+            setStatus("SUCCESS");
+        } else{
+            setStatus("REJECTED");
+        }
+    }
+    public Payment(String id, Order order, String method, Map<String, String> paymentData, String status) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        setStatus(status);
     }
     public void setStatus(String status){
-        return;
+        if (status.equals("SUCCESS") || status.equals("REJECTED")){
+            this.status = status;
+            if (status.equals("SUCCESS")){
+                order.setStatus("SUCCESS");
+            } else {
+                order.setStatus("FAILED");
+            }
+        } else{
+            throw new IllegalArgumentException();
+        }
+
     }
 }
