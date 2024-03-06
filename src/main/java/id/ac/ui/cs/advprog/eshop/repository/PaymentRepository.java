6705c6fc8365a8d659e6a09bff63aa37a4fb032a
patrickSevans123 <patricksamuel2107@gmail.com>
2024-03-06 20:05:20 +0700

    [REFACTOR] change paymentData datatype to map

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,36 +3,23 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PaymentRepository {
-    private List<Payment> paymentData = new ArrayList<>();
+    private final Map<String, Payment> paymentMap = new HashMap<>();
 
     public Payment save(Payment payment) {
-        int i = 0;
-        for (Payment savedPayment : paymentData){
-            if (savedPayment.getId().equals(payment.getId())){
-                paymentData.remove(i);
-                paymentData.add(i, payment);
-                return payment;
-            }
-            i += 1;
-        }
-
-        paymentData.add(payment);
+        paymentMap.put(payment.getId(), payment);
         return payment;
     }
 
     public Payment findById(String id) {
-        for (Payment payment : paymentData) {
-            if (payment.getId().equals(id)) {
-                return payment;
-            }
-        }
-        return null;
+        return paymentMap.get(id);
     }
 
     public List<Payment> getAllPayments() {
-        return new ArrayList<>(paymentData);
+        return new ArrayList<>(paymentMap.values());
     }
 }
