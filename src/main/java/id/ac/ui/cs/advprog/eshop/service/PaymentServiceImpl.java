db1a6fe7efb3ae3b8b5219e0d187802a5e50cb6a
patrickSevans123 <patricksamuel2107@gmail.com>
2024-03-06 20:13:12 +0700

    [RED] add paymentservice and paymentserviceimpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        return null;
+    }
+
+    @Override
+    public void setStatus(Payment payment, String status) {
+    }
+
+    @Override
+    public Payment getPayment(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return null;
+    }
+}
